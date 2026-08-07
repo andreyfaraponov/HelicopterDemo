d0bcac42c9a929d41fb9dfef846bbb0d9c6777f4
afarapon <andrey.faraponov@gmail.com>
2026-08-07 17:25:34 +0300

    added rear properller animation

---
+++ Assets/Scripts/RearMotorAnimator.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+
+namespace HelicopterDemo
+{
+    public class RearMotorAnimator : MonoBehaviour
+    {
+        [SerializeField] private Transform rearMotor;
+        [SerializeField] private float rotationSpeed;
+
+        private bool _isRotating;
+        private float _degPerSecond;
+        private Coroutine _rotateCoroutine;
+
+        private void Start()
+        {
+            StartRotate();
+        }
+
+        public void StartRotate()
+        {
+            _isRotating = true;
+            _rotateCoroutine = StartCoroutine(RotatePropellerCoroutine());
+        }
+
+        public void StopRotate()
+        {
+            StopCoroutine(_rotateCoroutine);
+            _isRotating = false;
+        }
+
+        private IEnumerator RotatePropellerCoroutine()
+        {
+            _degPerSecond = rotationSpeed * 360 / 60;
+            while (_isRotating)
+            {
+                rearMotor.Rotate(Vector3.right, _degPerSecond * Time.deltaTime, Space.Self);
+                yield return null;
+            }
+        }
+    }
+}
